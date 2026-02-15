17be1cfb70cf640d9a913842a2438ed123d80f36
Darshan <shetedarshan2005@gmail.com>
2026-02-15 09:45:44 +0530

    Day-22:smallest window containg 0,1and 2 sec method

--- Day-22/min_len_of_substring_0_1_2.cpp
+++ Day-22/min_len_of_substring_0_1_2.cpp
@@ -5,35 +5,75 @@
 using namespace std;
 
 // first method
-int min_len_substring( string &nums) {
-    int n = nums.length();
-    int zeroindex = 0,oneindex =0,twoindex = 0;
-    bool zero = false,one = false,two = false;
-    int res = 9999999;
-    for(int i =0; i<n; i++) {
-        if(nums[i] == '0') {
-            zeroindex = i;
-            zero = true;
-        }
-        else if(nums[i] == '1') {
-            oneindex = i;
-            one = true;
-        }
-        else if(nums[i] == '2') {
-            twoindex = i;
-            two = true;
-        }
-        if(zero and two and one) {
-            res = min(res, max({zeroindex, oneindex, twoindex}) - min({zeroindex, oneindex, twoindex}));
-        }
-    }
-    if(res == 9999999 ) return -1;
-    else return res+1;
+// int min_len_substring( string &nums) {
+//     int n = nums.length();
+//     int zeroindex = 0,oneindex =0,twoindex = 0;
+//     bool zero = false,one = false,two = false;
+//     int res = 9999999;
+//     for(int i =0; i<n; i++) {
+//         if(nums[i] == '0') {
+//             zeroindex = i;
+//             zero = true;
+//         }
+//         else if(nums[i] == '1') {
+//             oneindex = i;
+//             one = true;
+//         }
+//         else if(nums[i] == '2') {
+//             twoindex = i;
+//             two = true;
+//         }
+//         if(zero and two and one) {
+//             res = min(res, max({zeroindex, oneindex, twoindex}) - min({zeroindex, oneindex, twoindex}));
+//         }
+//     }
+//     if(res == 9999999 ) return -1;
+//     else return res+1;
 
+// }
+
+// int main() {
+//     string s = "012112";
+//     int n = min_len_substring(s);
+//     cout<<n;
+// }
+
+
+// second method using sliding window
+
+// Online C++ compiler to run C++ program online
+// #include <iostream>
+// #include<algorithm>
+// #include<string>
+// using namespace std;
+
+int min_len_subString(string &s) {
+    int n = s.length();
+    int i =0,k =0,cnt = 0,min_len = 999999;
+    int freq[3] = {0};
+    while(k<n) {
+      freq[s[k] - '0']++;
+      if(freq[s[k] - '0'] == 1) cnt++;
+      if(cnt == 3) {
+          while(freq[s[i] - '0'] > 1) {
+              freq[s[i] - '0']--;
+              i++;
+          }
+          min_len = min(min_len,k-i+1);
+          freq[s[i] - '0']--;
+          i++;
+          cnt--;
+      }
+      k++;
+    }
+    if(min_len == 999999) return -1;
+    else return min_len;
 }
 
 int main() {
-    string s = "012112";
-    int n = min_len_substring(s);
-    cout<<n;
+   string s = "1021211212";
+   int n = min_len_subString(s);
+   cout<<n;
+
+    return 0;
 }
--- Day-22/readMe.md
+++ Day-22/readMe.md
@@ -1,2 +1,8 @@
 # Smallest window containing 0, 1 and 2
 ##### first find in this problem our substring having 0,1,2 for that we use every index variable if 1 exist then we change to true do for alls 0 and one also then we calculate index at every time and we calculate len when all exist in subarray and finding max - min of there index max is for last index like 2 find at some index at 5 then all three condtion true and min is our first true is start and cal difference between then and add +1 then give this our len and comare with prev and upadte this is logic
+
+
+
+# second approch is using sliding windo
+##### first we have check all three values are exist or not for this we use freq array and of size 3 contain 0,1,2 index and update index every time and using i and k pointer we slide the windo k for next and i for shrinking then first we update freq then check this elemnt occour at ones then we increase count then if count is 3 then we conclude that all are inclues in string then for recheck some elemnt in i sides are dulicates so usinf while we moves i to last of duplicates and aslo update freq aftert that we update min_len with new len and update freq then update i and last we update k
+
